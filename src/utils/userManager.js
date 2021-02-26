419efd581cc752dd765dbf6e903623dd47e8c769
Nir Krevner <nir.krevner@gmail.com>
2021-02-26 11:15:04 +0200

    accounts.kab.info

--- src/utils/userManager.js
+++ src/utils/userManager.js
@@ -5,7 +5,7 @@ const userManagerConfig = {
   redirect_uri: `${window.location.protocol}//${window.location.hostname}${window.location.port ? `:${window.location.port}` : ''}/callback`,
   response_type: 'token id_token',
   scope: 'profile',
-  authority: 'https://accounts.kbb1.com/auth/realms/main',
+  authority: 'https://accounts.kab.info/auth/realms/main',
   silent_redirect_uri: `${window.location.protocol}//${window.location.hostname}${window.location.port ? `:${window.location.port}` : ''}/silent_renew.html`,
   automaticSilentRenew: true,
   filterProtocolClaims: true,
